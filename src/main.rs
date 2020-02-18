9abfab21ede0c65ec547a4da6cc93b2ff4cc14c4
Heather Etheridge <heathernetheridge@gmail.com>
2020-02-17 18:59:13 -0600

    will now loop through program until user wins the game

--- src/main.rs
+++ src/main.rs
@@ -10,33 +10,40 @@ fn main() {
     // gen_range includes the first param and is up to but not including 
     // the second param so this will generate a number between 1 and 100
     let secret_number = rand::thread_rng().gen_range(1, 101);
-
+    
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess: ");
+    // loop to give user multiple chances at guessing the number
+    loop {
+        println!("Please input your guess: ");
 
-    // gets a guess from the user and prints it 
-    let mut guess = String::new();
+        // gets a guess from the user and prints it 
+        let mut guess = String::new();
 
-    // calling the stdin function from the io module
-    // saves users answer to a mutable reference of guess
-    io::stdin().read_line(&mut guess)
-    //  read_line returns a Result type which is used in .expect
-        .expect("Failed to read line");
+        // calling the stdin function from the io module
+        // saves users answer to a mutable reference of guess
+        io::stdin().read_line(&mut guess)
+        //  read_line returns a Result type which is used in .expect
+            .expect("Failed to read line");
 
-    // converts user input string into a real number
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
-    
-     
-    println!("You guessed: {}", guess);
+        // converts user input string into a real number
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
+        
+            
+        println!("You guessed: {}", guess);
 
-    // match expression is made up of arms. 
-    // an arm consist of a pattern
-    // Rust compares the value given to match to each arms patern in turn
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        // match expression is made up of arms. 
+        // an arm consist of a pattern
+        // Rust compares the value given to match to each arms patern in turn
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+
     }
 }
